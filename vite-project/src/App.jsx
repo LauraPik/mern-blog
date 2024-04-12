a9e7e2889e7a85a0b1d737ca38651f25be41aeea
LauraPik <145454382+LauraPik@users.noreply.github.com>
2024-04-12 15:44:10 +0300

    New

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,16 +1,33 @@
 
-
+import Home from "./pages/Home"
+import About from "./pages/About"
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import DashBoard from "./pages/DashBoard";
+import Projects from "./pages/Projects";
+import { BrowserRouter, Route, Routes } from "react-router-dom"
 
 function App() {
 
-  return (
-    <>
-      <div className="text-3xl text-red-500">
-       BOO
-      </div>
+  
+    
+    return (
+      <BrowserRouter basename="/">
+        <Routes>
+          <Route path="/" element={<Home/>} /> 
+          <Route path="/about" element={<About/>} /> 
+          <Route path="/dashboard" element={<DashBoard/>}/>
+          <Route path="projects" element={<Projects/>}/>
+          <Route path="/signin" element={<SignIn/>}/>
+          <Route path="/signup" element={<SignUp/>}/>
+         
+
+        </Routes>
+      </BrowserRouter>
+    );
       
-    </>
-  )
+   
+  
 }
 
 export default App
---
+++ vite-project/src/pages/About.jsx
@@ -0,0 +1,8 @@
+
+const About = ()=>{
+    return(
+        <div>Labas</div>
+    )
+}
+
+export default About;
